468213da6d4b667695c6ec4fc35b89251d493335
wq-max <53320735+wq-max@users.noreply.github.com>
2021-12-26 14:34:13 +0800

    chapter5完结

---
+++ chapter5/src/resources/ResourceTest.java
@@ -0,0 +1,24 @@
+package resources;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class ResourceTest {
+
+    public static void main(String[] args) throws IOException {
+        Class cl = ResourceTest.class;
+        URL aboutURL = cl.getResource("about.gif");
+        var icon = new ImageIcon(aboutURL);
+
+        InputStream stream = cl.getResourceAsStream("about.txt");
+        var about = new String(stream.readAllBytes(), "UTF-8");
+
+        InputStream stream2 = cl.getResourceAsStream("title.txt");
+        var title = new String(stream2.readAllBytes(), StandardCharsets.UTF_8).trim();
+
+        JOptionPane.showMessageDialog(null, about, title, JOptionPane.INFORMATION_MESSAGE, icon);
+    }
+}
