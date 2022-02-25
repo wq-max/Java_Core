a58353ee1e8343b2a426a73ab0c3c24e5502e41d
wq-max <53320735+wq-max@users.noreply.github.com>
2022-02-25 10:49:00 +0800

    chapter 6 end

---
+++ chapter6/src/anonymousInnerClass/AnonymousInnerClassTest.java
@@ -0,0 +1,34 @@
+package anonymousInnerClass;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
+
+public class AnonymousInnerClassTest {
+
+    public static void main(String[] args) {
+        var clock = new TalkingClock();
+        clock.start(1000, true);
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+class TalkingClock{
+    public void start(int interval, boolean beep){
+        var listener = new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("At the tone, the time is "
+                        + Instant.ofEpochMilli(e.getWhen()));
+                if (beep) Toolkit.getDefaultToolkit().beep();
+            }
+        };
+        var timer = new Timer(interval, listener);
+        timer.start();
+    }
+}
