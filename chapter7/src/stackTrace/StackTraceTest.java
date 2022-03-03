4767d559ff125746344d06481fd083dbca221c1d
wq-max <53320735+wq-max@users.noreply.github.com>
2022-03-03 10:57:08 +0800

    chapter7 异常end

---
+++ chapter7/src/stackTrace/StackTraceTest.java
@@ -0,0 +1,25 @@
+package stackTrace;
+
+import java.util.Scanner;
+
+public class StackTraceTest {
+
+    public static void main(String[] args) {
+        try(var in = new Scanner(System.in)) {
+            System.out.print("Enter n:");
+            int n = in.nextInt();
+            factorial(n);
+        }
+    }
+
+    public static int factorial(int n){
+        System.out.println("factorial(" + n + ")");
+        var walker = StackWalker.getInstance();
+        walker.forEach(System.out::println);
+        int r;
+        if (n <= 1) r = 1;
+        else r = n * factorial(n - 1);
+        System.out.println("return " + r);
+        return r;
+    }
+}
